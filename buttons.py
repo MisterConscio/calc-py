bb2aaaf7de6752a2ce1ac9630263ba3b17ef4b30
MisterConscio <saulo.costam@gmail.com>
2023-08-29 18:23:49 -0300

    Updating display with equation

--- buttons.py
+++ buttons.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QGridLayout, QPushButton
 
 from config import FS_400
 from display import Display
+from info import Info
 from utils import isEmpty, isNumOrDot, isValidNumber
 
 
@@ -19,10 +20,16 @@ class Button(QPushButton):
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, display: Display, *args, **kwargs):
+    def __init__(self, display: Display, info: Info, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.display = display
+        self.info = info
+        self._equation = ""
+        self.equation = "Equação"
+        self._left = None
+        self._right = None
+        self._op = None
 
         self._gridMask = [
             ["C", "►", "^", "/"],
@@ -34,25 +41,48 @@ class ButtonsGrid(QGridLayout):
 
         self._makeGrid()
 
+    @property
+    def equation(self):
+        return self._equation
+
+    @equation.setter
+    def equation(self, value):
+        self._equation = value
+        self.info.setText(self._equation)
+
     def _makeGrid(self):
         for i, row in enumerate(self._gridMask):
             for j, btn_text in enumerate(row):
                 btn = Button(btn_text)
+
                 if not isNumOrDot(btn_text) and not isEmpty(btn_text):
                     btn.setProperty("cssClass", "specialButton")
-                self.addWidget(btn, i, j)
-                btn.clicked.connect(
-                    self._makeBtnDisplaySlot(self._insertTextTodisplay, btn)
-                )
+                    self._configSpecialBtn(btn)
 
-    def _makeBtnDisplaySlot(self, func, *args, **kwargs):
+                self.addWidget(btn, i, j)
+                slot = self._makeSlot(self._insertTextToDisplay, btn)
+                self._connectBtnClicked(btn, slot)
+
+    def _connectBtnClicked(self, btn, slot):
+        btn.clicked.connect(slot)
+
+    def _configSpecialBtn(self, btn):
+        text = btn.text()
+
+        if text == "C":
+            self._connectBtnClicked(btn, self._clear)
+
+        if text in "+-/*":
+            self._connectBtnClicked(btn, self._makeSlot(self._operatorClicked, btn))
+
+    def _makeSlot(self, func, *args, **kwargs):
         @Slot(bool)
-        def realSlot(_):
-            func(_, *args, **kwargs)
+        def realSlot():
+            func(*args, **kwargs)
 
         return realSlot
 
-    def _insertTextTodisplay(self, checked, button):
+    def _insertTextToDisplay(self, button):
         btnText = button.text()
         newDisplayValue = self.display.text() + btnText
 
@@ -60,3 +90,25 @@ class ButtonsGrid(QGridLayout):
             return
 
         self.display.insert(btnText)
+
+    def _clear(self):
+        self._left = None
+        self._right = None
+        self._op = None
+        self.equation = "Equação"
+        self.display.clear()
+
+    def _operatorClicked(self, btn):
+        btnText = btn.text()
+        displayText = self.display.text()
+        self.display.clear()
+
+        if not isValidNumber(displayText) and self._left is None:
+            print("Nenhum número foi digitado ainda")
+            return
+
+        if self._left is None:
+            self._left = float(displayText)
+
+        self._op = btnText
+        self.equation = f"{self._left} {self._op} ??"
--- main.py
+++ main.py
@@ -21,19 +21,8 @@ if __name__ == "__main__":
     display = Display()
     window.addWidgetToVLayout(display)
 
-    btnGrid = ButtonsGrid(display)
+    btnGrid = ButtonsGrid(display, info)
     window.v_layout.addLayout(btnGrid)
-
-    # btnGrid.addWidget(Button("1"), 0, 0)
-    # btnGrid.addWidget(Button("2"), 0, 1)
-    # btnGrid.addWidget(Button("3"), 0, 2)
-    # btnGrid.addWidget(Button("4"), 1, 0)
-    # btnGrid.addWidget(Button("5"), 1, 1)
-    # btnGrid.addWidget(Button("6"), 1, 2)
-    # btnGrid.addWidget(Button("7"), 2, 0)
-    # btnGrid.addWidget(Button("8"), 2, 1)
-    # btnGrid.addWidget(Button("9"), 2, 2)
-    # btnGrid.addWidget(Button("0"), 3, 1, 3, 1)
 
     window.adjsutFixedSize()
     window.show()
