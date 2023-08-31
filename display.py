f5ad036eef504154d293218d20a2d75180b656c3
MisterConscio <saulo.costam@gmail.com>
2023-08-31 11:30:49 -0300

    Adding key events to display

--- display.py
+++ display.py
@@ -1,10 +1,13 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 
 from config import FS_700, MIN_WIDTH, PADDING
 
 
 class Display(QLineEdit):
+    eqResquested = Signal()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
@@ -14,4 +17,14 @@ class Display(QLineEdit):
         self.setMinimumHeight(FS_700 * 2)
         self.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.setMinimumWidth(MIN_WIDTH)
-        # self.setTextMargins(PADDING, PADDING, PADDING, PADDING)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        key = event.key()
+
+        isEnter = key in [Qt.Key.Key_Enter, Qt.Key.Key_Return]
+
+        if isEnter:
+            self.eqResquested.emit()
+            return event.ignore()
+
+        # return super().keyPressEvent(event)
